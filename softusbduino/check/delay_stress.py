e904e2ff47f79bc491574ddabe28d1eea33da9e7
ponty <ponty@home>
2012-04-02 19:12:34 +0200

    fix for py25

--- softusbduino/check/delay_stress.py
+++ softusbduino/check/delay_stress.py
@@ -18,7 +18,7 @@ def main(
          ):
     mcu = Arduino(auto_reconnect=False)
 
-    TEMPL = 'count={count:>5} fail={fail:>5} wait={wait:>5} ms in {func} int={interrupts} sleep={sleep_between_calls} s'
+#    TEMPL = 'count={count:>5} fail={fail:>5} wait={wait:>5} ms in {func} int={interrupts} sleep={sleep_between_calls} s'
     count = 0
     fail = 0
     skip = int(2 / sleep_between_calls + 0.5)
@@ -26,7 +26,7 @@ def main(
         count += 1
         try:
             if count % skip == 0:
-                print TEMPL.format(
+                print dict(
                                wait=1000 * wait,
                                func=func,
                                interrupts=interrupts,
@@ -39,7 +39,7 @@ def main(
                              func,
                              interrupts=interrupts)
             time.sleep(sleep_between_calls)
-            assert mcu.usb_transfer(42) == 42
+            mcu.firmware_test()
             
         except (usb.USBError, ArduinoUsbDeviceError) , e:
             print('USBError: %s reconnect_time: %s s' % (str(e), reconnect_time))
