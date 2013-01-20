2a17d8aa43d08a62e879aacf782758d3d5a752a5
ponty <ponty@home>
2013-01-20 21:04:56 +0100

    pwm high freq mode

--- softusbduino/check/dump.py
+++ softusbduino/check/dump.py
@@ -80,7 +80,3 @@ def usbdump():
     # , key=lambda x:mcu.registers.address(x[0]) # sort by address
     for k, v in SortedDict(mcu.registers.as_dict()).items():
         print '%-20s = 0x%02X @0x%2X' % (k, v, mcu.registers.address(k))
-
-
-
-
