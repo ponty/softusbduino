a145d244c252053e5dc5888300ae3960eb72d0b2
ponty <ponty@home>
2014-02-17 14:01:19 +0100

    demo improved

--- softusbduino/examples/onewire_demo.py
+++ softusbduino/examples/onewire_demo.py
@@ -1,24 +1,37 @@
 from entrypoint2 import entrypoint
 from softusbduino.arduino import Arduino
 import time
+from softusbduino.onewire import OneWireError
 
+
+def search(mcu, pins):
+    alldevs = dict()
+    for p in pins:
+        try:
+            print('searching on pin:%s'% p)
+            bus = mcu.onewire.bus(p)
+            devs = bus.search()
+            for d in devs:
+                print('device found:')
+                print('  address=%s'% d.address_str)
+                print('  address_valid=%s'% d.address_valid)
+                print('  chip=%s'% d.chip)
+                print('  resolution=%s bit'% d.resolution)
+                alldevs[d.address_str] = d
+        except OneWireError, e:
+            print('OneWireError', e)
+
+    return alldevs
 
 @entrypoint
 def main(
-    pin='D8',
+    pins='A0,A1,A2,D5,D6,D7',
     timeout=10,
 ):
+    pins=pins.split(',')
     mcu = Arduino()
-#    mcu.watchdog.start(8)
-    bus = mcu.onewire.bus(pin)
     print '--> search'
-    devs = bus.search()
-    for d in devs:
-        print 'device found:'
-        print '  address=', d.address_str
-        print '  address_valid=', d.address_valid
-        print '  chip=', d.chip
-        print '  resolution=', d.resolution, 'bit'
+    devs = search(mcu, pins).values()
 
     print '--> read'
     start = time.time()
